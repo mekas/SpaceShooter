fa72d3537be28b52e5e538783478c888bc7a4ed5
mekas <eka.suryana@gmail.com>
2016-10-27 05:23:27 +0700

    first commit

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+        if(other.tag == "Player")
+        {
+            Instantiate(playerExplosion, transform.position, transform.rotation);
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+    public float tumble;
+    private Rigidbody rb;
+    private Vector3 rotSpeed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Vector3.Scale(Random.insideUnitSphere, new Vector3(1,1,1)) * tumble;
+        //rotSpeed = Vector3.Scale(Random.insideUnitSphere, new Vector3(1,0,1)) * tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Rotate(rotSpeed);
+    }
+}
